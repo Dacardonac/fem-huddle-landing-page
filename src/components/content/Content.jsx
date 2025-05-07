32d5eca85bc1a0e6460e70d89bc2208249039569
Daniel Alejandro Cano Cardona <dacardonac201@gmail.com>
2025-05-07 12:45:46 -0500

    style: Update styles for App, Content, and RegisterBtn components; add missing styles for Paragraph and Title components

--- src/components/content/Content.jsx
+++ src/components/content/Content.jsx
@@ -1,6 +1,7 @@
 import Title from "../title/Title";
 import Paragraph from "../paragraph/Paragraph";
 import RegisterBtn from "../registerBtn/Register";
+import './Content.scss'
 
 const Content = () => {
   return (
